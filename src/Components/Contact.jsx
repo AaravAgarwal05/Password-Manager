74d4f4bcbaf7a18081c68d44db12121c354c6709
Aarav Agarwal <aarav.knp.08@gmail.com>
2024-04-29 01:17:23 +0530

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 const Contact = () => {
   return (
     <>
-      <div className="text-white absolute inset-0 -z-10 h-full items-center px-5 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      <div className="text-white absolute inset-0 -z-10 sm:h-full h-fit items-center px-5 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
         <Navbar />
         <div className="flex flex-col container mx-auto gap-5 h-4/6">
           <h1 className="font-bold text-2xl">Get in Touch</h1>
